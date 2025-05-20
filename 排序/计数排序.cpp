58b06a8f8dbd3472606c430f352b5675ca7e43b8
Yao Guanxiong <guanxiongme@gmail.com>
2025-05-20 23:25:05 +0800

    0520

---
+++ 排序/计数排序.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+void Sort(vector<int> &nums)
+{
+    int m = 0;
+    for (int num : nums)
+    {
+        m = max(m, num);
+    }
+    vector<int> counter(m + 1, 0);
+    for (int num : nums)
+    {
+        counter[num]++;
+    }
+    int i = 0;
+    for (int num = 0; num <= m; num++)
+    {
+        for (int j = 0; j < counter[num]; j++, i++)
+        {
+            nums[i] = num;
+        }
+    }
+}
+int main()
+{
+    int m;
+    cin >> m;
+    vector<int> nums(m);
+    for (int i = 0; i < m; i++)
+    {
+        cin >> nums[i];
+    }
+    Sort(nums);
+    for (int i = 0; i < m; i++)
+    {
+        cout << nums[i] << " ";
+    }
+    return 0;
+}
