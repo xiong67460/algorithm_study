65d802533d602b926dc61415cf5702bb353c47f5
Yao Guanxiong <guanxiongme@gmail.com>
2025-05-28 00:15:13 +0800

    0527

--- test/test.cpp
+++ test/test.cpp
@@ -1,33 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-const int MAX = 200010;
-int f[MAX];
-
 int main()
 {
-    int n, m;
-    cin >> n >> m;
-    fill(f, f + MAX, -1); 
-    queue<int> q;
-    q.push(n);
-    f[n] = 0;
-    while (!q.empty())
-    {
-        int cur = q.front();
-        q.pop();
-        if (cur == m)
-            break;
-        int nexts[3] = {cur - 1, cur + 1, cur * 2};
-        for (int i = 0; i < 3; ++i)
-        {
-            int nxt = nexts[i];
-            if (nxt >= 0 && nxt < MAX && f[nxt] == -1)
-            {
-                f[nxt] = f[cur] + 1;
-                q.push(nxt);
-            }
-        }
-    }
-    cout << f[m] << endl;
+    return 0;
 }
