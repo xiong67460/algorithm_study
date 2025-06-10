dff9f5d83f40261e2ab5b99a76eb224b5d8b5e10
xiong67460 <guanxiongme@gmail.com>
2025-06-10 14:03:03 +0800

    0610

--- sth/test-lc.cpp
+++ sth/test-lc.cpp
@@ -1,1 +1,22 @@
 #include "../utils/common.hpp"
+int solve(vector<vector<int>> &nums, int n)
+{   if(n==1)
+        return 0;
+    int res = INT_MAX;
+    for (auto &num : nums)
+    {
+        if (num[1]==n)
+            res = min(res, solve(nums, n - 1));
+    }
+    return res;
+}
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> nums;
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < 3; j++)
+            cin >> nums[i][j];
+    cout << solve(nums, m);
+}
